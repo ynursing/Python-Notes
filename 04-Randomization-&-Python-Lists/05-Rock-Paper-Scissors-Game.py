131fa61397cde79b9d2f756a7886f7df1cc25ff2
ynursing <hnursing24@gmail.com>
2025-04-18 20:45:25 +0400

    Added RPS game

---
+++ 04-Randomization-&-Python-Lists/05-Rock-Paper-Scissors-Game.py
@@ -0,0 +1,49 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+user_input = int(input('Welcome to Rock, Paper, Scissors challenge.'
+                       'Type 0 for Rock, 1 for Paper, 2 for Scissors.\n'))
+moves = [rock, paper, scissors]
+print(moves[user_input])
+print('The computer chose:')
+computer_choice = random.choice(moves)
+print(computer_choice)
+
+if user_input == 2 and computer_choice == moves[2]:
+    print("It's a draw.")
+elif user_input == 1 and computer_choice == moves[1]:
+    print("It's a draw.")
+elif user_input == 0 and computer_choice == moves[0]:
+    print("It's a draw.")
+elif user_input == 0 and computer_choice == moves[2]:
+    print("You win!")
+elif user_input == 1 and computer_choice == moves[0]:
+    print("You win!")
+elif user_input == 2 and computer_choice == moves[1]:
+    print("You win!")
+else:
+    print("You lose!")
