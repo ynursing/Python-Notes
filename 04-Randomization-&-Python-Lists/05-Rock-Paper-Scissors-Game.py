ea1994ff5f920fc14a8a350f8cc96e624eb4a51f
ynursing <hnursing24@gmail.com>
2025-04-21 16:20:12 +0400

    Fixed RPS game

--- 04-Randomization-&-Python-Lists/05-Rock-Paper-Scissors-Game.py
+++ 04-Randomization-&-Python-Lists/05-Rock-Paper-Scissors-Game.py
@@ -28,22 +28,20 @@ scissors = '''
 user_input = int(input('Welcome to Rock, Paper, Scissors challenge.'
                        'Type 0 for Rock, 1 for Paper, 2 for Scissors.\n'))
 moves = [rock, paper, scissors]
-print(moves[user_input])
-print('The computer chose:')
-computer_choice = random.choice(moves)
-print(computer_choice)
-
-if user_input == 2 and computer_choice == moves[2]:
-    print("It's a draw.")
-elif user_input == 1 and computer_choice == moves[1]:
-    print("It's a draw.")
-elif user_input == 0 and computer_choice == moves[0]:
-    print("It's a draw.")
-elif user_input == 0 and computer_choice == moves[2]:
-    print("You win!")
-elif user_input == 1 and computer_choice == moves[0]:
-    print("You win!")
-elif user_input == 2 and computer_choice == moves[1]:
-    print("You win!")
+if user_input < 0 or user_input >= 3:
+    print("Invalid option; Computer wins by default.")
 else:
-    print("You lose!")
+    print(moves[user_input])
+    print('The computer chose:')
+    computer_choice = random.choice(moves)
+    print(computer_choice)
+    if user_input == moves.index(computer_choice):
+        print("It's a draw.")
+    elif user_input == 0 and computer_choice == moves[2]:
+        print("You win!")
+    elif user_input == 1 and computer_choice == moves[0]:
+        print("You win!")
+    elif user_input == 2 and computer_choice == moves[1]:
+        print("You win!")
+    else:
+        print("You lose!")
